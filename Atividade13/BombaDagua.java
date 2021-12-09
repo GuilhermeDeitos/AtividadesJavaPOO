1ec97ad4c15b66f56fdea815d0db3d6a0ca4cb12
Guilherme Augusto Deitos Alves <65348931+GuilhermeDeitos@users.noreply.github.com>
2021-12-09 19:37:02 -0300

    Add files via upload

---
+++ Atividade13/BombaDagua.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.TimeUnit;
+
+public class BombaDagua {
+  public boolean status;
+
+  public void ligar(){
+    status = true;
+  }
+  public void desligar(){
+    status = false;
+  }
+  public void mostrar(){
+    if(status){
+      System.out.println("A bomba está ligada");
+    } else if(status == false){
+      System.out.println("A bomba está desligada");
+    } else{
+      System.out.println("Pane no sistema, alguém me desconfigurou 😮😮😮");
+    }
+    
+  }
+  public void mostrarTempo(int tempo){
+    for(int i = tempo; i >= 0; i--){
+      try {
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(i);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+     
+    }
+   
+  }
+}
